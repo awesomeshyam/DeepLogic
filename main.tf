0a16eddd567535b893ac8ee3e041644455135644
awesomeshyam <awesomeshyam91@gmail.com>
2025-01-02 14:58:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "rock-rg"
   location = "centralindia"
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rocky-rg"
+  location = "centralindia"
+}
